c2699986c596d5eb0b73cf188667718608610489
hushin <ngtv.hsn@gmail.com>
2024-01-08 23:12:58 +0900

    update hono

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-import { Hono } from 'hono'
-import { poweredBy } from 'hono/powered-by'
+import { Hono } from 'hono';
+import { poweredBy } from 'hono/powered-by';
 
-const app = new Hono()
+const app = new Hono();
 
-app.use('*', poweredBy())
+app.use('*', poweredBy());
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-export default app
+app.get('/post/:id/:commentId', (c) => {
+  const { id, commentId } = c.req.param();
+  return c.text(`Hello Hono! ${id} ${commentId}`);
+});
+
+export default app;
