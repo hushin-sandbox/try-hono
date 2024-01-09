4eeeb027790609c7218085a908cc56071a7ac964
hushin <ngtv.hsn@gmail.com>
2024-01-10 00:23:16 +0900

    create counter

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import { poweredBy } from 'hono/powered-by';
 
-const app = new Hono();
+type Bindings = {
+  COUNTERS: KVNamespace;
+};
 
+const app = new Hono();
 app.use('*', poweredBy());
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
 });
 
-app.get('/post/:id/:commentId', (c) => {
-  const { id, commentId } = c.req.param();
-  return c.text(`Hello Hono! ${id} ${commentId}`);
+const api = new Hono<{ Bindings: Bindings }>();
+app.use('/api/*', cors());
+
+api.get('/counter', async (c) => {
+  const key = 'visits';
+  let count = Number(await c.env.COUNTERS.get(key)) || 0;
+  count++;
+  await c.env.COUNTERS.put(key, count.toString());
+
+  return c.text(count.toString());
 });
+app.route('/api', api);
 
 export default app;
